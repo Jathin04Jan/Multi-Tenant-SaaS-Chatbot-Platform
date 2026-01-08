caf9d6d2dc3d406bc2e776aebf32fdd052846b67
Jathin Narayan <jathin04official@gmail.com>
2026-01-08 21:09:41 +0530

    celery job to fetch minio file

--- backend/component_learning/celery-learning/tasks.py
+++ backend/component_learning/celery-learning/tasks.py
@@ -9,15 +9,26 @@ def process(a, b):
     while i < 10:
         sleep(1)
         i += 1
-        print("processing......")
+        print("processing task 1......")
     c = a + b
-    result = subprocess(c)
-    return "given to subprocess"
+    result = subprocess("task - 1 ")
+    return "given to subprocess 1"
 
-def subprocess(c):
-    print("subprocessing......")
-    sleep(2)
-    return c + 2
+def subprocess(task_name):
+    print(f"subprocessing {"task_name"} task......")
+    sleep(3)
+
+@app.task
+def process2(b, c):
+    print("This is process 2")
+    i = 0
+    while i < 10:
+        sleep(1)
+        i += 1
+        print("processing task 2......")
+    result = subprocess("task - 2 ")
+    return "given to subprocess 2"
+    
 
 if __name__ == '__main__':
     #result = process.delay(1, 2)
